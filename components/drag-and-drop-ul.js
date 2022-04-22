33889d76c27d616c98d20e1e72fb84de2f23e8e9
Joshua Martin <the.manuscriptmaster@gmail.com>
2022-04-22 17:07:26 -0400

    Make accessible

--- components/drag-and-drop-ul.js
+++ components/drag-and-drop-ul.js
@@ -1,5 +1,5 @@
 import { html } from 'https://unpkg.com/lit-html@2.2.2/lit-html.js';
-import { useState } from '../utils/hooks.js';
+import { useEffect, useState } from '../utils/hooks.js';
 import { reactiveElement } from '../utils/reactive-element.js';
 
 const handleDragStart = (event, index) => {
@@ -18,26 +18,58 @@ const moveItem = (list, from, to) => {
   return _list;
 };
 
-export const DragAndDropUl = reactiveElement([], () => {
+export const DragAndDropUl = reactiveElement([], ({ host }) => {
   const [items, setItems] = useState(['blue', 'green', 'red']);
+  const [tab, setTab] = useState(0);
+  const [allowMove, setAllowMove] = useState(false);
+  useEffect(() => {
+    if (
+      host.shadowRoot.activeElement &&
+      host.shadowRoot.querySelectorAll('li')[tab] !==
+        host.shadowRoot.activeElement
+    ) {
+      host.shadowRoot.querySelectorAll('li')[tab].focus();
+    }
+  }, [tab, host.shadowRoot.activeElement]);
 
   const handleDrop = (event, index) => {
     event.preventDefault();
     const from = parseInt(event.dataTransfer.getData('text/plain'), 10);
     const to = index;
     setItems(moveItem(items, from, to));
+    setTab(to);
   };
 
-  const handleKeyDown = (event, index) => {
+  const handleKeyUp = (event, index) => {
     event.preventDefault();
     switch (event.code) {
       case 'ArrowUp':
-        return index !== 0 && setItems(moveItem(items, index, index - 1));
+        if (index > 0 && allowMove) {
+          const from = index;
+          const to = index - 1;
+          setItems(moveItem(items, from, to));
+          setTab(to);
+        }
+        break;
       case 'ArrowDown':
-        return (
-          index !== items.length - 1 &&
-          setItems(moveItem(items, index, index + 1))
-        );
+        if (index < items.length - 1 && allowMove) {
+          const from = index;
+          const to = index + 1;
+          setItems(moveItem(items, from, to));
+          setTab(to);
+        }
+        break;
+      case 'Tab':
+        if (host.shadowRoot.activeElement.getAttribute('draggable')) {
+          const tab = Array.from(
+            host.shadowRoot.querySelectorAll('[draggable="true"]')
+          ).indexOf(host.shadowRoot.activeElement);
+          setTab(tab);
+        }
+        break;
+      case 'Space':
+        setAllowMove(!allowMove);
+        break;
     }
   };
 
@@ -57,18 +89,24 @@ export const DragAndDropUl = reactiveElement([], () => {
         color: white;
       }
     </style>
-    <ul>
+    <button @click="${console.log}">This should be tabbable first</button>
+    <h1 id="h1">
+      Use the arrow key to move element higher or lower in priority
+    </h1>
+    <ul role="listbox">
       ${items.map(
         (item, index) => html`<li
+          aria-describedby="h1"
+          role="option"
+          tabindex="0"
           style="background-color: ${item}"
           draggable="true"
-          tabindex="0"
-          @keyup="${(event) => handleKeyDown(event, index)}"
+          @keyup="${(event) => handleKeyUp(event, index)}"
           @dragstart="${(event) => handleDragStart(event, index)}"
           @dragover="${(event) => handleDragOver(event)}"
           @drop="${(event) => handleDrop(event, index)}"
         >
-          Box
+          ${item} box
         </li>`
       )}
     </ul>
