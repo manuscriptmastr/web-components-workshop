ad41a51c879f43c79b1d0916208182386a030657
Joshua Martin <the.manuscriptmaster@gmail.com>
2022-07-11 08:33:20 -0400

    Revert "Initial example"
    This reverts commit 321e9089ea138c0b8f7df927ef281f97ba69ca15.

--- components/form.js
+++ components/form.js
@@ -1,11 +1,10 @@
 import { html } from 'lit-html';
-import { connect, update } from '../store/index.js';
-import { useEffect } from '../utils/hooks.js';
+import { useEffect, useState } from '../utils/hooks.js';
 import { reactiveElement } from '../utils/reactive-element.js';
 
-export const Form = reactiveElement([], ({ origin, roaster }) => {
-  const setOrigin = (origin) => update((state) => ({ ...state, origin }));
-  const setRoaster = (roaster) => update((state) => ({ ...state, roaster }));
+export const Form = reactiveElement([], () => {
+  const [origin, setOrigin] = useState('Worka, Ethiopia');
+  const [roaster, setRoaster] = useState('Madcap Coffee Company');
   useEffect(() => {
     console.log('<app-form> mounting effect');
     return () => console.log('<app-form> unmounting effect');
@@ -26,7 +25,4 @@ export const Form = reactiveElement([], ({ origin, roaster }) => {
   `;
 });
 
-customElements.define(
-  'app-form',
-  connect(({ origin, roaster }) => ({ origin, roaster }), Form)
-);
+customElements.define('app-form', Form);
