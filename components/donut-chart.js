eec5df63140fb043622f26528a63b2949a657743
Joshua Martin <the.manuscriptmaster@gmail.com>
2022-07-12 12:50:02 -0400

    Rephrase problem to make everything in units of 100

--- components/donut-chart.js
+++ components/donut-chart.js
@@ -5,68 +5,50 @@ export class DonutChart extends ReactiveElement {
   render() {
     return html`
       <style>
-        :host {
-          --pi: 3.14159265359;
-          --radius: 14;
-          --circumference: calc(2 * var(--pi) * var(--radius));
-        }
-
         svg {
           display: inline-block;
           height: 200px;
           width: 200px;
         }
 
+        g {
+          transform: rotate(0.4turn);
+          transform-origin: 50% 50%;
+        }
+
         circle {
-          --units: 0;
-          --offset: 0;
           fill: none;
-          stroke: currentColor;
           stroke-linecap: round;
-          stroke-dasharray: calc(var(--circumference) * var(--units) / 100 - 3)
-            999;
-          stroke-dashoffset: calc(
-            var(--circumference) * var(--offset) / 100 * -1
-          );
           stroke-width: 2;
-        }
-
-        #slice-1 {
-          --units: 35;
-          color: orange;
-        }
-
-        #slice-2 {
-          --units: 15;
-          --offset: 35;
-          color: green;
-        }
-
-        #slice-3 {
-          --units: 20;
-          --offset: 50;
-          color: red;
-        }
-
-        #slice-4 {
-          --units: 30;
-          --offset: 70;
-          color: gray;
         }
 
         #text {
           font-size: 0.5rem;
         }
       </style>
-      <svg viewBox="0 0 30 30">
-        <circle id="slice-1" r="14" cx="15" cy="15"></circle>
-        <circle id="slice-2" r="14" cx="15" cy="15"></circle>
-        <circle id="slice-3" r="14" cx="15" cy="15"></circle>
-        <circle id="slice-4" r="14" cx="15" cy="15"></circle>
+      <svg viewBox="0 0 34 34">
+        <g>
+          <circle
+            r="15.91549430918954"
+            cx="17"
+            cy="17"
+            stroke="orange"
+            stroke-dasharray="70 999"
+            stroke-dashoffset="0"
+          ></circle>
+          <!-- <circle
+            r="15.91549430918954"
+            cx="17"
+            cy="17"
+            stroke="blue"
+            stroke-dasharray="28 999"
+            stroke-dashoffset="-71"
+          ></circle> -->
+        </g>
         <text
           id="text"
-          x="15"
-          y="15"
+          x="17"
+          y="17"
           text-anchor="middle"
           alignment-baseline="central"
         >
