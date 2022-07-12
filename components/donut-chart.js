745fcb7bc4fa0431641c8d90610ddf977f94c838
Joshua Martin <the.manuscriptmaster@gmail.com>
2022-07-12 13:55:41 -0400

    Tweaks

--- components/donut-chart.js
+++ components/donut-chart.js
@@ -18,24 +18,36 @@ export class DonutChart extends ReactiveElement {
 
         circle {
           fill: none;
+          /* Note that stroke-linecap and stroke-width must be factored in when divided up available space */
           stroke-linecap: round;
           stroke-width: 2;
         }
 
-        #text {
+        text {
           font-size: 0.5rem;
         }
       </style>
       <svg viewBox="0 0 34 34">
+        <!-- Segments are grouped and rotated so that zero begins at a different point of the circle -->
         <g>
+          <!-- Radius is set so that circumference equals 100 absolute units -->
           <circle
             r="15.91549430918954"
             cx="17"
             cy="17"
             stroke="orange"
-            stroke-dasharray="70 999"
+            stroke-dasharray="30 999"
             stroke-dashoffset="0"
           ></circle>
+          <circle
+            r="15.91549430918954"
+            cx="17"
+            cy="17"
+            stroke="green"
+            stroke-dasharray="37 999"
+            stroke-dashoffset="-33"
+          ></circle>
+          <!-- Area we plan not to use: -->
           <!-- <circle
             r="15.91549430918954"
             cx="17"
@@ -45,13 +57,8 @@ export class DonutChart extends ReactiveElement {
             stroke-dashoffset="-71"
           ></circle> -->
         </g>
-        <text
-          id="text"
-          x="17"
-          y="17"
-          text-anchor="middle"
-          alignment-baseline="central"
-        >
+        <!-- Centered text -->
+        <text x="17" y="17" text-anchor="middle" alignment-baseline="central">
           10
         </text>
       </svg>
